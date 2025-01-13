d4a6b353c2fd592cff2a6535d92a47da9780b1fd
Adalberto <adalberto8648@gmail.com>
2025-01-12 21:48:04 -0300

    telefone

--- cadastro_clientes.py
+++ cadastro_clientes.py
@@ -6,7 +6,7 @@
 
 
 # Lista onde vamos armazenar os dados dos clientes temporariamente
-clientes = []
+lista_clientes_vazia = []
 
 # Coletando as informações do cliente
 while True:
@@ -25,31 +25,41 @@ while True:
     # Verificando se existe erro, colocando texto onde seria apenas números
     try:
         # Inserindo idade do cliente
-        idade = int(input('Digite aqui a idade do cliente: '))
-        # Removendo espaços antes e depois e se não está vazio
+        idade = int(input("Digite aqui a idade do cliente: "))
+        # Verificando se número é maior que 0
         if idade > 0:
             break
         else:
-            print('Digite apenas números maiores que zero para a idade do cliente')
-    # Retornando a mensagem
+            print("Digite apenas números maiores que zero para a idade do cliente")
+    # Exibe a mensagem de erro se tiver digitado letra ou vazio
     except ValueError:
         print('Digite apenas números para a idade do cliente')
 
-
-telefone = input('Digite aqui o telefone do cliente: ')
+while True:
+    # Verificando se existe erro, colocando texto onde seria apenas números
+    try:
+        # Inserindo telefone do cliente
+        telefone = input("Digite aqui o telefone do cliente: ")
+        # Fazendo contagem de número quer deve ser 11
+        if len(telefone) != 11:
+            print('O telefone deve ter 11 dígitos. Tente novamente')
+            continue # aqui é pra continuar e voltar no início
+        # Formatando o telefone
+        telefone_formatado = f"({telefone[:2]}) {telefone[2:7]}-{telefone[7:]}"
+    except ValueError:
+        print("Digite apenas números para o telefone do cliente")
 
 # Armazenando as informações em um dicionário
 cliente = {
     'Nome' : nome_completo,
     'Idade' : idade,
-    'Telefone' : telefone
+    'Telefone' : telefone_formatado
 }
 
 # Adicionando o dicionário de cliente na lista de clientes provisório
-clientes.append(cliente)
+lista_clientes_vazia.append(cliente)
 
 # Exibindo as informações do cliente cadastrado
-
 print('\nCliente cadastrado com sucesso')
 print(f'Nome: {cliente['Nome']}')
 print(f'Idade: {cliente["Idade"]}')
